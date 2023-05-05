219a76bf9b9ddbbcd73c767b70dc3f8034930714
Perry <62119804+perrylets@users.noreply.github.com>
2023-05-04 23:44:49 -0300

    Use `CompareTag` instead of `==`
    Use `CompareTag` instead of `==`

--- Assets/Etra Games/Etra'sStarterAssets/1-UserAssets/InteractableObjects/WeightedButton/WeightedButton.cs
+++ Assets/Etra Games/Etra'sStarterAssets/1-UserAssets/InteractableObjects/WeightedButton/WeightedButton.cs
@@ -2,77 +2,77 @@ using UnityEngine;
 
 namespace Etra.StarterAssets.Interactables
 {
-    public class WeightedButton : MonoBehaviour
+  public class WeightedButton : MonoBehaviour
+  {
+    //From Krissy#1337
+    /*
+    The MIT License (MIT)
+    Copyright 2023 Krissy
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    */
+
+    //References
+    public SciFiDoor door;
+    private Transform buttonMain;
+
+    //Variables
+    Vector3 buttonStartPos = new Vector3(0, -0.94f, 0);
+    Vector3 buttonEndPos = new Vector3(0, -1.32f, 0);
+    //int numObjects = 0; // Track the number of objects on the button
+
+    //Set buttonStartPos
+    private void Start()
     {
-        //From Krissy#1337
-        /*
-        The MIT License (MIT)
-        Copyright 2023 Krissy
-        Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-        The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-        */
-
-        //References
-        public SciFiDoor door;
-        private Transform buttonMain;
-
-        //Variables
-        Vector3 buttonStartPos = new Vector3(0, -0.94f, 0);
-        Vector3 buttonEndPos = new Vector3(0, -1.32f, 0);
-        //int numObjects = 0; // Track the number of objects on the button
-
-        //Set buttonStartPos
-        private void Start()
-        {
-            buttonMain = transform.GetChild(0);
-            var buttonStartPos = buttonMain.transform.position;
-        }
-
-        //Check for player or object collision
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.tag == "Player")
-            {
-                //numObjects++; // New object on button
-                buttonPressed();
-            }
-            else if (other.GetComponent<Rigidbody>() != null)
-            {
-                //  numObjects++; // New object on button
-                buttonPressed();
-            }
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            doorClose();
-        }
-
-        public void removeObject()
-        {
-            //   numObjects--;
-        }
-
-        public void doorClose()
-        {
-            removeObject();
-            //  if (numObjects == 0)
-            buttonReleased();
-        }
-
-        public void buttonPressed()
-        {
-            door.SetOpened(true);
-            //Use LeanTween to play basic animations
-            LeanTween.moveLocal(buttonMain.gameObject, buttonEndPos, 0.15f);
-        }
-
-        public void buttonReleased()
-        {
-            door.SetOpened(false);
-            //Use LeanTween to play basic animations
-            LeanTween.moveLocal(buttonMain.gameObject, buttonStartPos, 0.15f);
-        }
+      buttonMain = transform.GetChild(0);
+      var buttonStartPos = buttonMain.transform.position;
     }
+
+    //Check for player or object collision
+    private void OnTriggerEnter(Collider other)
+    {
+      if (other.gameObject.CompareTag("Player"))
+      {
+        //numObjects++; // New object on button
+        buttonPressed();
+      }
+      else if (other.GetComponent<Rigidbody>() != null)
+      {
+        //  numObjects++; // New object on button
+        buttonPressed();
+      }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+      doorClose();
+    }
+
+    public void removeObject()
+    {
+      //   numObjects--;
+    }
+
+    public void doorClose()
+    {
+      removeObject();
+      //  if (numObjects == 0)
+      buttonReleased();
+    }
+
+    public void buttonPressed()
+    {
+      door.SetOpened(true);
+      //Use LeanTween to play basic animations
+      LeanTween.moveLocal(buttonMain.gameObject, buttonEndPos, 0.15f);
+    }
+
+    public void buttonReleased()
+    {
+      door.SetOpened(false);
+      //Use LeanTween to play basic animations
+      LeanTween.moveLocal(buttonMain.gameObject, buttonStartPos, 0.15f);
+    }
+  }
 }
