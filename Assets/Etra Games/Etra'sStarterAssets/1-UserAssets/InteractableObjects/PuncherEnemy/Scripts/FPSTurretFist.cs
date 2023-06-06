78dcd85d8399c7f23d38199179a70b65201fbbe9
Perry <perry@perrylets.dev.br>
2023-06-05 23:55:40 -0300

    Damage the player wwhen the turret hits and if the health system is present

--- Assets/Etra Games/Etra'sStarterAssets/1-UserAssets/InteractableObjects/PuncherEnemy/Scripts/FPSTurretFist.cs
+++ Assets/Etra Games/Etra'sStarterAssets/1-UserAssets/InteractableObjects/PuncherEnemy/Scripts/FPSTurretFist.cs
@@ -1,41 +1,46 @@
 ﻿using System.Collections;
+using Etra.StarterAssets.Abilities;
 using UnityEngine;
 
 namespace Etra.StarterAssets.Interactables.Enemies
 {
-  public class FPSTurretFist : MonoBehaviour
-  {
-    public FPSTurret mainScript;
-    bool cooldown;
-    private void OnTriggerEnter(Collider other)
+    public class FPSTurretFist : MonoBehaviour
     {
-      //If the player is hit, launch them back
-      if (other.gameObject.CompareTag("Player"))
-      {
-        mainScript.launchPlayer(other.gameObject);
-      }
-
-      //If a damageable thing is hit (like another puncher), deal damage to it
-      if (!cooldown)
-      {
-        var isDamageableCheck = other.gameObject.GetComponent<IDamageable<int>>();
-        if (isDamageableCheck != null)
+        public FPSTurret mainScript;
+        bool cooldown;
+        private void OnTriggerEnter(Collider other)
         {
-          isDamageableCheck.TakeDamage(1);
-          //Wait one second before applying damage again with that fist
-          StartCoroutine(friendlyFireCooldown());
+            //If the player is hit, launch them back
+            if (other.gameObject.CompareTag("Player"))
+            {
+                mainScript.launchPlayer(other.gameObject);
+                if (other.GetComponent<ABILITY_ContinuousHealth>())
+                {
+                    other.GetComponent<ABILITY_ContinuousHealth>().Damage();
+                }
+            }
+
+            //If a damageable thing is hit (like another puncher), deal damage to it
+            if (!cooldown)
+            {
+                var isDamageableCheck = other.gameObject.GetComponent<IDamageable<int>>();
+                if (isDamageableCheck != null)
+                {
+                    isDamageableCheck.TakeDamage(1);
+                    //Wait one second before applying damage again with that fist
+                    StartCoroutine(friendlyFireCooldown());
+                }
+            }
+
         }
-      }
+
+
+        IEnumerator friendlyFireCooldown()
+        {
+            cooldown = true;
+            yield return new WaitForSeconds(1);
+            cooldown = false;
+        }
 
     }
-
-
-    IEnumerator friendlyFireCooldown()
-    {
-      cooldown = true;
-      yield return new WaitForSeconds(1);
-      cooldown = false;
-    }
-
-  }
 }
