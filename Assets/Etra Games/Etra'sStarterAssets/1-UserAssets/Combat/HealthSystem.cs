075c19ad08614d05390bd10f374355e887cb7f97
Perry <perry@perrylets.dev.br>
2023-06-15 10:40:06 -0300

    Add better validation

--- Assets/Etra Games/Etra'sStarterAssets/1-UserAssets/Combat/HealthSystem.cs
+++ Assets/Etra Games/Etra'sStarterAssets/1-UserAssets/Combat/HealthSystem.cs
@@ -8,16 +8,14 @@ namespace Etra.StarterAssets.Combat
     {
         #region Variables
         [Header("Variables")]
-        [field: SerializeField, Tooltip("The health of the GameObject")]
+        [SerializeField, Tooltip("The health of the GameObject")]
         float _health;
-        /**
-          <summary>
-            The health of the GameObject.
-          </summary>
-          <remarks>
-            This variable's setter isn't public because it should be set by using the <see cref="Damage"/> and <see cref="Heal"/> methods.
-          </remarks>
-        */
+        /// <summary>
+        ///   The health of the GameObject.
+        /// </summary>
+        /// <remarks>
+        ///   This variable's setter isn't public because it should be set by using the <see cref="Damage"/> and <see cref="Heal"/> methods.
+        /// </remarks>
         public float health
         {
             get
@@ -28,9 +26,9 @@ namespace Etra.StarterAssets.Combat
             {
                 float lastHealth = _health;
                 _health = Mathf.Clamp(value, 0, maxHealth);
-                float result = Mathf.Abs(_health - lastHealth);
-                if (result != 0)
-                    OnChange?.Invoke(result);
+                float diff = Mathf.Abs(_health - lastHealth);
+                if (diff != 0)
+                    OnChange?.Invoke(diff);
                 if (manualDeath)
                     return;
 
@@ -40,86 +38,72 @@ namespace Etra.StarterAssets.Combat
             }
         }
         [Tooltip("The maximum health of the object")] public float maxHealth;
-        /**
-          <summary>
-            If true, <see cref="OnDeath"/> won't be called.
-          </summary>
-        */
+        /// <summary>
+        ///   If true, <see cref="OnDeath"/> won't be called.
+        /// </summary>
         [Tooltip("If true, the OnDeath event won't be called")] public bool manualDeath;
         public bool isAlive { get; private set; }
 
         #endregion
 
         #region Events
-        /**
-          <summary>
-            Called when the object is damaged.
-          </summary>
-          <remarks>
-            If the health is set to the value it already has, this event won't be executed.
-          </remarks>
-        */
+        /// <summary>
+        ///   Called when the object is damaged.
+        /// </summary>
+        /// <remarks>
+        ///   If the health is set to the value it already has, this event won't be executed.
+        /// </remarks>
         [Header("Events")]
         [Tooltip("Called when the health changes")] public UnityEvent<float> OnChange;
-        /**
-          <summary>
-            Called when the object is damaged.
-          </summary>
-          <remarks>
-            If the damage dealed by <see cref="Damage"/> is equal to 0 or the <paramref name="skipEvents"/> parameter is true, this event won't be executed.
-          </remarks>
-        */
+        /// <summary>
+        ///   Called when the object is damaged.
+        /// </summary>
+        /// <remarks>
+        ///   If the damage dealed by <see cref="Damage"/> is equal to 0 or the <paramref name="skipEvents"/> parameter is true, this event won't be executed.
+        /// </remarks>
         [Tooltip("Called when the object is damaged")] public UnityEvent<float> OnDamage;
-        /**
-          <summary>
-            Called when the object is healed.
-          </summary>
-          <remarks>
-            If the health healed by <see cref="Heal"/> is equal to 0 or the <paramref name="skipEvents"/> parameter is true, this event won't be executed.
-          </remarks>
-        */
+        /// <summary>
+        ///   Called when the object is healed.
+        /// </summary>
+        /// <remarks>
+        ///   If the health healed by <see cref="Heal"/> is equal to 0 or the <paramref name="skipEvents"/> parameter is true, this event won't be executed.
+        /// </remarks>
         [Tooltip("Called when the object is healed")] public UnityEvent<float> OnHeal;
-        /**
-          <summary>
-            Called when the health becomes 0.
-          </summary>
-        */
+        /// <summary>
+        ///   Called when the health becomes 0.
+        /// </summary>
         [Tooltip("Called when the health becomes 0")] public UnityEvent OnDeath;
 
         #endregion
 
         #region Methods
 
-        /**
-          <summary>
-            Decreases the amount of health.
-          </summary>
-          <param name="hp">Amount of health to decrease. Negative values will be evaluated as positive.</param>
-          <param name="skipEvents">If true, <see cref="OnDamage"/> won't be called</param>
-        */
+        /// <summary>
+        ///   Decreases the amount of health
+        /// </summary>
+        /// <param name="hp">Amount of health to decrease. Negative values will be evaluated as positive.</param>
+        /// <param name="skipEvents">If true, <see cref="OnDamage"/> won't be called</param>
         public void Damage(float hp, bool skipEvents = false)
         {
             float lastHealth = health;
             health -= Mathf.Abs(hp);
-            float result = Mathf.Abs(_health - lastHealth);
-            if (result != 0 || !skipEvents)
-                OnDamage?.Invoke(result);
+            float diff = Mathf.Abs(_health - lastHealth);
+            if (diff != 0 || !skipEvents)
+                OnDamage?.Invoke(diff);
         }
 
-        /**
-          <summary>
-            Increases the amount of health.
-          </summary>
-          <param name="hp">Amount of health to increase. Negative values will be evaluated as positive.</param>
-          <param name="skipEvents">If true, <see cref="OnHeal"/> won't be called</param>
-        */
+        /// <summary>
+        ///   Increases the amount of health
+        /// </summary>
+        /// <param name="hp">Amount of health to increase. Negative values will be evaluated as positive.</param>
+        /// <param name="skipEvents">If true, <see cref="OnHeal"/> won't be called</param>
         public void Heal(float hp, bool skipEvents = false)
         {
             float lastHealth = health;
             health += Mathf.Abs(hp);
-            float result = Mathf.Abs(_health - lastHealth);
-            if (result != 0 || !skipEvents)
-                OnHeal?.Invoke(result);
+            float diff = Mathf.Abs(_health - lastHealth);
+            if (diff != 0 || !skipEvents)
+                OnHeal?.Invoke(diff);
         }
 
         #endregion
@@ -128,7 +112,8 @@ namespace Etra.StarterAssets.Combat
 
         void OnValidate()
         {
-            health = Mathf.Clamp(health, 0, maxHealth);
+            _health = Mathf.Clamp(health, 0, maxHealth);
+            maxHealth = Mathf.Max(maxHealth, 0);
         }
 
         #endregion
