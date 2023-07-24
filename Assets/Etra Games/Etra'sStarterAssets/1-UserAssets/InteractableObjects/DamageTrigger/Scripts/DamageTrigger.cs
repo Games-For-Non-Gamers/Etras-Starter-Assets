68ef04de01f16bcb7a224ff666161a318c1f7e97
Perry <perry@perrylets.dev.br>
2023-07-23 23:37:34 -0300

    Finish the damage trigger

--- Assets/Etra Games/Etra'sStarterAssets/1-UserAssets/InteractableObjects/DamageTrigger/Scripts/DamageTrigger.cs
+++ Assets/Etra Games/Etra'sStarterAssets/1-UserAssets/InteractableObjects/DamageTrigger/Scripts/DamageTrigger.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using Etra.StarterAssets;
 using Etra.StarterAssets.Abilities;
+using Etra.StarterAssets.Combat;
 
 [RequireComponent(typeof(MeshRenderer), typeof(MeshFilter), typeof(Collider))]
 public class DamageTrigger : MonoBehaviour
@@ -10,17 +11,22 @@ public class DamageTrigger : MonoBehaviour
     [SerializeField] bool showInGame = false;
 
     MeshRenderer meshRenderer;
+#if UNITY_EDITOR
+    HealthSystem healthSystem;
+#endif
 
     void Reset()
     {
         meshRenderer = GetComponent<MeshRenderer>();
     }
+
     void OnValidate()
     {
         meshRenderer = GetComponent<MeshRenderer>();
 
         meshRenderer.enabled = showInEditor;
     }
+
     void Awake()
     {
         meshRenderer = GetComponent<MeshRenderer>();
@@ -29,13 +35,37 @@ public class DamageTrigger : MonoBehaviour
     void Start()
     {
         meshRenderer.enabled = showInGame;
+#if UNITY_EDITOR
+        healthSystem = EtraCharacterMainController.Instance.etraAbilityManager.GetComponentInChildren<HealthSystem>();
+        if (healthSystem != null)
+        {
+            healthSystem.OnChange.AddListener(Log);
+        }
+#endif
+
     }
+
+#if UNITY_EDITOR
+    void OnDestroy()
+    {
+        if (healthSystem != null)
+        {
+            healthSystem.OnChange.RemoveListener(Log);
+        }
+    }
+
+    void Log(float changed)
+    {
+        Debug.Log($"Current health: {healthSystem.health} Changed health: {changed}");
+    }
+#endif
 
     void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             EtraCharacterMainController.Instance.etraAbilityManager.GetComponent<ABILITY_ContinuousHealth>()?.Damage();
+            //TODO: add support for hp based health when it's implemented
         }
     }
 }
