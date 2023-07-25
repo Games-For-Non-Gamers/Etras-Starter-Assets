9f1149dcd683d6c62e989336d9985587a9bd539d
Perry <perry@perrylets.dev.br>
2023-07-24 23:25:26 -0300

    Fix the damage filter

--- Assets/Etra Games/Etra'sStarterAssets/1-UserAssets/CharacterAsset/Scripts/Abilities/General_Abilities/ABILITY_ContinuousHealth.cs
+++ Assets/Etra Games/Etra'sStarterAssets/1-UserAssets/CharacterAsset/Scripts/Abilities/General_Abilities/ABILITY_ContinuousHealth.cs
@@ -10,31 +10,36 @@ namespace Etra.StarterAssets.Abilities
     [AbilityUsage(EtraCharacterMainController.GameplayTypeFlags.All, AbilityUsageAttribute.AbilityTypeFlag.Passive, typeof(HealthSystem)), RequireComponent(typeof(ABILITY_CheckpointRespawn))]
     class ABILITY_ContinuousHealth : EtraAbilityBaseClass
     {
-        [SerializeField]
+        [SerializeField, Header("Main variables")]
         AnimationCurve damageCurve;
         [SerializeField]
         float damageCooldownWaitTime, healCooldownWaitTime;
         bool damageCooldown = false, healCooldown = false;
+        [SerializeField, Header("Misc"), Tooltip("The index of the damage filter inside the canvas")]
+        int siblingIndex = 1;
         int currentStep = 0;
         HealthSystem healthSystem;
         ABILITY_CheckpointRespawn checkpointRespawn;
         Image image;
+        float animationTime;
         public override void abilityStart()
         {
             healthSystem = GetComponentInChildren<HealthSystem>();
-            checkpointRespawn = GetComponent<ABILITY_CheckpointRespawn>();
             healthSystem.OnDamage.AddListener(OnDamage);
             healthSystem.OnHeal.AddListener(OnHeal);
             healthSystem.OnDeath.AddListener(OnDeath);
             healthSystem.OnChange.AddListener(OnChange);
+            checkpointRespawn = GetComponent<ABILITY_CheckpointRespawn>();
+            animationTime = checkpointRespawn.animationTime * .5f;
             image = EtrasResourceGrabbingFunctions
                 .addPrefabFromAssetsByName("DamageFilter", EtraCharacterMainController.Instance.starterAssetCanvas.transform, false, Vector3.zero, new Quaternion(0, 0, 0, 0), new Vector3(1, 1, 0))
                 .GetComponent<Image>();
+            image.transform.SetSiblingIndex(siblingIndex);
         }
 
         public void Damage()
         {
-            if (damageCooldown) return;
+            if (damageCooldown || healthSystem.manualDeath) return;
 
             currentStep = currentStep % damageCurve.keys.Length;
             float damage = damageCurve.keys[currentStep].value;
@@ -44,7 +49,7 @@ namespace Etra.StarterAssets.Abilities
 
         public void Heal(float health)
         {
-            if (healCooldown) return;
+            if (healCooldown || healthSystem.manualDeath) return;
 
             healthSystem.Heal(health);
         }
@@ -62,16 +67,15 @@ namespace Etra.StarterAssets.Abilities
 
         void OnDeath()
         {
+            healthSystem.manualDeath = true;
             checkpointRespawn.teleportToCheckpoint();
-            healthSystem.Heal(healthSystem.maxHealth, true);
-            currentStep = 0;
+            StartCoroutine(Respawn());
         }
 
         void OnChange(float change)
         {
             float healthLost = healthSystem.maxHealth - healthSystem.health;
             image.color = new Color(image.color.r, image.color.g, image.color.b, healthLost / healthSystem.maxHealth);
-            Debug.Log(image.color.a);
         }
 
         IEnumerator DamageCooldown()
@@ -86,6 +90,14 @@ namespace Etra.StarterAssets.Abilities
             healCooldown = true;
             yield return new WaitForSeconds(healCooldownWaitTime);
             healCooldown = false;
+        }
+
+        IEnumerator Respawn()
+        {
+            yield return new WaitForSeconds(animationTime);
+            healthSystem.Heal(healthSystem.maxHealth, true);
+            currentStep = 0;
+            healthSystem.manualDeath = false;
         }
     }
 }
