883eb2c2365de3d4d623c1a65c2e779197bab80b
Perry <perry@perry.dev.br>
2023-08-20 17:14:15 -0300

    Refactor damage dealing for DamageTrigger

--- Assets/Etra Games/Etra'sStarterAssets/1-UserAssets/InteractableObjects/DamageTrigger/Scripts/DamageTrigger.cs
+++ Assets/Etra Games/Etra'sStarterAssets/1-UserAssets/InteractableObjects/DamageTrigger/Scripts/DamageTrigger.cs
@@ -3,73 +3,76 @@ using Etra.StarterAssets;
 using Etra.StarterAssets.Abilities;
 using Etra.StarterAssets.Combat;
 
-[RequireComponent(typeof(MeshRenderer), typeof(MeshFilter), typeof(Collider))]
-public class DamageTrigger : MonoBehaviour
+namespace Etra.StarterAssets.Interactables
 {
-    [SerializeField, Tooltip("Amount of damage to deal")]
-    float damage = 25f;
-    [Header("Rendering")]
-    [SerializeField] bool showInEditor = true;
-    [SerializeField] bool showInGame = false;
+    [RequireComponent(typeof(MeshRenderer), typeof(MeshFilter), typeof(Collider))]
+    public class DamageTrigger : MonoBehaviour
+    {
+        [SerializeField, Tooltip("Amount of damage to deal")]
+        float damage = 25f;
+        [Header("Rendering")]
+        [SerializeField] bool showInEditor = true;
+        [SerializeField] bool showInGame = false;
 
-    MeshRenderer meshRenderer;
+        MeshRenderer meshRenderer;
 #if UNITY_EDITOR
-    HealthSystem healthSystem;
+        HealthSystem healthSystem;
 #endif
 
-    void Reset()
-    {
-        meshRenderer = GetComponent<MeshRenderer>();
-    }
-
-    void OnValidate()
-    {
-        meshRenderer = GetComponent<MeshRenderer>();
-
-        meshRenderer.enabled = showInEditor;
-    }
-
-    void Awake()
-    {
-        meshRenderer = GetComponent<MeshRenderer>();
-    }
-
-    void Start()
-    {
-        meshRenderer.enabled = showInGame;
-#if UNITY_EDITOR
-        healthSystem = EtraCharacterMainController.Instance.etraAbilityManager.GetComponentInChildren<HealthSystem>();
-        if (healthSystem != null)
+        void Reset()
         {
-            healthSystem.OnChange.AddListener(Log);
+            meshRenderer = GetComponent<MeshRenderer>();
         }
-#endif
 
-    }
-
-#if UNITY_EDITOR
-    void OnDestroy()
-    {
-        if (healthSystem != null)
+        void OnValidate()
         {
-            healthSystem.OnChange.RemoveListener(Log);
+            meshRenderer = GetComponent<MeshRenderer>();
+
+            meshRenderer.enabled = showInEditor;
         }
-    }
 
-    void Log(float changed)
-    {
-        Debug.Log($"Current health: {healthSystem.health} Changed health: {changed}");
-    }
-#endif
-
-    void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        void Awake()
         {
-            if (EtraCharacterMainController.Instance.etraAbilityManager.TryGetComponent<ABILITY_Health>(out var ability))
+            meshRenderer = GetComponent<MeshRenderer>();
+        }
+
+        void Start()
+        {
+            meshRenderer.enabled = showInGame;
+#if UNITY_EDITOR
+            healthSystem = EtraCharacterMainController.Instance.etraAbilityManager.GetComponentInChildren<HealthSystem>();
+            if (healthSystem != null)
             {
-                ability.Damage(damage);
+                healthSystem.OnChange.AddListener(Log);
             }
+#endif
+
+        }
+
+#if UNITY_EDITOR
+        void OnDestroy()
+        {
+            if (healthSystem != null)
+            {
+                healthSystem.OnChange.RemoveListener(Log);
+            }
+        }
+
+        void Log(float changed)
+        {
+            Debug.Log($"Current health: {healthSystem.health} Changed health: {changed}");
+        }
+#endif
+
+        void OnTriggerStay(Collider other)
+        {
+            if (!other.CompareTag("Player"))
+                return;
+
+            if (!EtraCharacterMainController.Instance.etraAbilityManager.TryGetComponent<ABILITY_Health>(out ABILITY_Health ability))
+                return;
+
+            ability.Damage(damage);
         }
     }
 }
